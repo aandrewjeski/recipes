857da9e3fefa563ba721c5405373ee8e321d7597
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-06-02 08:43:28 -0400

    Added bootstrap (template search, random, nav - currently placeholder)

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'pg'
 require 'pry'
 require 'will_paginate'
 require 'will_paginate/array'
-require 'will_paginate/active_record'
+#require 'will_paginate/active_record'
 
 def db_connection
   begin
